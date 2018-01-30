399280af3f7cea9262598f14306b04a1629b6c63
EarthenSky <gabegames27@gmail.com>
2018-01-29 20:51:58 -0800

    my premade gameloop preset ^^

---
+++ SDL_2_TEST_GAME/SDL_2_TEST_GAME/GameLoop.cpp
@@ -0,0 +1,61 @@
+#include <SDL.h>
+#include <stdio.h>
+
+void RenderAssets(SDL_Renderer *renderer);
+void HandleInput();
+void closeMedia();
+
+SDL_Event gInputHandler;  //handles different types of input
+
+const int STOPPED = 0;
+const int RUNNING = 1;
+const int PAUSED = 2;
+int gGameState = RUNNING;
+
+void GameLoop(SDL_Renderer *renderer) 
+{
+	//main GameLoop:
+	while (gGameState != STOPPED) {
+		if (gGameState == RUNNING) {
+			RenderAssets(renderer);  //renders textures.
+
+			//regular game code here
+		}
+		else if (gGameState == PAUSED) {
+			//menu code here
+		}
+
+		HandleInput();  //also handles keyboard and mouse input.
+
+		//debug code here
+	}
+
+	closeMedia();
+}
+
+void RenderAssets(SDL_Renderer *renderer)
+{
+	//clear screen
+	SDL_RenderClear(renderer);
+
+	//render stuff here
+
+	//update screen
+	SDL_RenderPresent(renderer);
+}
+
+void HandleInput() 
+{
+	while (SDL_PollEvent(&gInputHandler) != 0) {
+		if (gInputHandler.type == SDL_QUIT) {
+			//i put (==) here instead of (=).  Litteraly the first time i've done this...
+			gGameState = STOPPED;  
+		}
+	}
+}
+
+//free textures and other media
+void closeMedia() 
+{
+
+}
---
+++ SDL_2_TEST_GAME/SDL_2_TEST_GAME/GameManager.cpp
@@ -0,0 +1,56 @@
+#include <SDL.h>
+#include <stdio.h>  //why?
+#include <string>  //incase I need strings.
+
+void GameLoop(SDL_Renderer *renderer);
+void finalClose();
+
+const int SCREEN_HEIGHT = 600;
+const int SCREEN_WIDTH = 600;
+
+SDL_Window *gWindow = NULL;  //null pointer is better than garbage pointer
+SDL_Renderer *gRenderer = NULL;  //NULL = 0 in most compilers
+
+int main(int argc, char* args[]) 
+{
+	printf("staatou! \n");
+
+	SDL_Init(SDL_INIT_VIDEO);
+
+	gWindow = SDL_CreateWindow(
+		"This is the name of the Window", 
+		SDL_WINDOWPOS_UNDEFINED, 
+		SDL_WINDOWPOS_UNDEFINED, 
+		SCREEN_WIDTH, 
+		SCREEN_HEIGHT, 
+		SDL_WINDOW_SHOWN
+	);
+
+	//vsync is on therefore psudeo infinite loops will run at around 60fps.
+	gRenderer = SDL_CreateRenderer(
+		gWindow, 
+		-1, 
+		SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC
+	);
+
+	SDL_SetRenderDrawColor(gRenderer, 0, 24, 78, 0);  //sets the render color to white.
+
+	GameLoop(gRenderer);
+
+	finalClose();
+	return 0;
+}
+
+void finalClose() 
+{
+	//Destroy renderer
+	SDL_DestroyRenderer(gRenderer);
+	gRenderer = NULL;
+
+	//Destroy window
+	SDL_DestroyWindow(gWindow);
+	gWindow = NULL;
+
+	SDL_Quit();
+
+}
