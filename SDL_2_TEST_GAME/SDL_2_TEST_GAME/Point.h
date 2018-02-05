4997b5de5752e235ca81a62f6ab92285a429f7d6
EarthenSky <gabegames27@gmail.com>
2018-02-04 23:09:18 -0800

    opertator test

--- SDL_2_TEST_GAME/SDL_2_TEST_GAME/Point.h
+++ SDL_2_TEST_GAME/SDL_2_TEST_GAME/Point.h
@@ -4,8 +4,18 @@
 //holds two int values
 struct Point {
 	int x, y;
+
+	Point operator+(const Point &pnt) const;
 };
 
+Point Point::operator+ (const Point &pnt) const
+{
+	Point out;
+
+	out.x = (*this).x + pnt.x;
+	out.y = (*this).y + pnt.y;
+
+	return out;
+}
+
 #endif
-
-
